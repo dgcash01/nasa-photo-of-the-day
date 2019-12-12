f9872a16e883b03695f6388652fe9d9896d8a6fe
Dan Cashion <dangcashion@gmail.com>
2019-12-11 18:50:26 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Read through the instructions in the README.me
+      </p>
+    </div>
+  );
+}
+
+export default App;
